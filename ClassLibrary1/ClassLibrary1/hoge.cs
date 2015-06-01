1dd83849284fb2464a846560089ff69d4acd3cf4
木村 春揮 <h.kimura@powerworks.co.jp>
2015-06-01 16:52:55 +0900

    複製後コミット

--- ClassLibrary1/ClassLibrary1/hoge.cs
+++ ClassLibrary1/ClassLibrary1/hoge.cs
@@ -11,6 +11,7 @@ namespace ClassLibrary1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git!");
+            Console.WriteLine("Goodbye Git!");
         }
     }
 }
