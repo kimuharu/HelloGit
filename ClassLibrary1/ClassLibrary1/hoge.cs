314e3ac5b7ce3789ea85f91534241ea78eba9f4e
木村 春揮 <h.kimura@powerworks.co.jp>
2015-06-01 18:07:29 +0900

    6/1最後の更新

--- ClassLibrary1/ClassLibrary1/hoge.cs
+++ ClassLibrary1/ClassLibrary1/hoge.cs
@@ -12,6 +12,7 @@ namespace ClassLibrary1
         {
             Console.WriteLine("Hello Git!");
             Console.WriteLine("Goodbye Git!");
+            Console.WriteLine("power works");
         }
     }
 }
